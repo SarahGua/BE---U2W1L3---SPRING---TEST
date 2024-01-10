46ff682c21ae53808622480116e23bccde07954a
SarahGuarneri <sarahguarneri97@icloud.com>
2024-01-10 15:16:54 +0100

    FILES

---
+++ src/main/java/sarah/guarneri/BE/U2W1L3/BeU2W1L3Application.java
@@ -0,0 +1,13 @@
+package sarah.guarneri.BE.U2W1L3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W1L3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W1L3Application.class, args);
+	}
+
+}
---
+++ src/test/java/sarah/guarneri/BE/U2W1L3/BeU2W1L3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sarah.guarneri.BE.U2W1L3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W1L3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
